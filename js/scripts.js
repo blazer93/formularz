42e7a8e107ff6fa65179bb151a59111958559e14
blazer93 <83808989+blazer93@users.noreply.github.com>
2022-06-25 14:53:58 +0200

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -16,14 +16,15 @@ function verification(){
 }
 
 function goin(){
-    var nickElem = document.forms['login']['log'];
-    var passElem = document.forms['login']['pas'];
-
-if(nickElem.value.lenght < 3 ) {
+    const loginform = document.forms.login
+    const nickElem = loginform.elements['log'];
+    const passElem = loginform.elements['pas'];
+console.log(nickElem)
+if(nickElem.value.length < 3 ) {
     nickElem.style.backgroundColor = "red";
     return  false;
 }
-if(passElem.value.lenght < 6 ) {
+if(passElem.value.length < 6 ) {
     passElem.style.backgroundColor = "red";
     return false;
 
